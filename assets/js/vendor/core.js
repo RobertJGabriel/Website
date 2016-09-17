01dba9c67eb704e26699b2f8bba0daece7ddaa45
Robert James Gabriel <robert_gabriel@outlook.com>
2016-09-17 15:14:34 +0100

    Working code and colours for time

--- assets/js/vendor/core.js
+++ assets/js/vendor/core.js
@@ -4,26 +4,29 @@ $(function() {
     var navText = $("#logo1");
     var bannerText = $('#logo2');
     navText.addClass("show");
-    navText.text("Robert James Gabriel" );
+    navText.text("Robert James Gabriel");
     $(window).scroll(function() {
 
         var scroll = $(window).scrollTop();
 
-          if (scroll >= 100) {
-
-
-              bannerText.length > 0 ?  navText.text("Robert James Gabriel - " + bannerText.text()):navText.text("Robert James Gabriel" );
-          } else {
-
-
-                bannerText.length > 0 ? navText.text("Robert James Gabriel"):navText.text("Robert James Gabriel" );
+        if (scroll >= 100) {
+            bannerText.length > 0 ? navText.text("Robert James Gabriel - " + bannerText.text()) : navText.text("Robert James Gabriel");
+        } else {
+            bannerText.length > 0 ? navText.text("Robert James Gabriel") : navText.text("Robert James Gabriel");
         }
 
     });
 
 
+    if (bannerText.length > 0) {
 
 
+        var h1Elements = document.getElementsByTagName("h1");
+        var color = document.getElementById('header').style.backgroundColor;
+        for (var i = 1; i < h1Elements.length; i++) {
+            h1Elements[i].style.color = color;
+        }
+    }
     $(".menu-button").click(function(e) {
         $(".menu-bar").addClass("open");
         $(".grey").addClass("show");
@@ -36,7 +39,7 @@ $(function() {
     $(document).click(function(e) {
         if (!$(e.target).is('.menu-bar *,.menu-bar')) {
             $(".menu-bar").removeClass("open");
-                $(".grey").removeClass("show");
+            $(".grey").removeClass("show");
         }
     });
 });
