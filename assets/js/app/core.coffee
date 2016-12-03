c721e07468790e56735cb3b77793024eb962b2bb
Robert James Gabriel <robert_gabriel@outlook.com>
2016-12-03 00:54:29 +0000

    More

--- assets/js/app/core.coffee
+++ assets/js/app/core.coffee
@@ -12,6 +12,30 @@ $ ->
      console.log 'hi'
      $('#easteregg').modal 'show'
      return
+  if window.location.href.indexOf('/apps.html') > -1
+
+    $.ajax
+      type: 'get'
+      url: 'https://api.github.com' + '/users/robertjgabriel/repos'
+      data: type: 'all',
+      dataType: 'json'
+      success: (installations) ->
+        console.log(installations)
+        i = 0
+        while i < installations.length
+
+          installation = installations[i]
+          fullName = "<h3>" +  installation.full_name  + "</h3>"
+
+          i++
+          $('#gallery').append('<li><div class="panel panel-primary"><div class="panel-body">'+   fullName + '<p>'+  installation.description + ' <br/>  <b>Made Mostly with: '+ installation.language  + '</p><a href="'+ installation.url + '" class="btn btn-flat btn-warning">View Now</a></div></div></li>')
+
+        return
+      error: (error) ->
+        console.log error
+
+
+
 
   $(window).scroll ->
     scroll = $(window).scrollTop()
--- assets/js/app/core.js
+++ assets/js/app/core.js
@@ -12,6 +12,30 @@ $(function() {
     console.log('hi');
     $('#easteregg').modal('show');
   });
+  if (window.location.href.indexOf('/apps.html') > -1) {
+    $.ajax({
+      type: 'get',
+      url: 'https://api.github.com' + '/users/robertjgabriel/repos',
+      data: {
+        type: 'all',
+        dataType: 'json'
+      },
+      success: function(installations) {
+        var fullName, i, installation;
+        console.log(installations);
+        i = 0;
+        while (i < installations.length) {
+          installation = installations[i];
+          fullName = "<h3>" + installation.full_name + "</h3>";
+          i++;
+          $('#gallery').append('<li><div class="panel panel-primary"><div class="panel-body">' + fullName + '<p>' + installation.description + ' <br/>  <b>Made Mostly with: ' + installation.language + '</p><a href="' + installation.url + '" class="btn btn-flat btn-warning">View Now</a></div></div></li>');
+        }
+      },
+      error: function(error) {
+        return console.log(error);
+      }
+    });
+  }
   $(window).scroll(function() {
     var scroll;
     scroll = $(window).scrollTop();
