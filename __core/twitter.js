e66e0137a7f4e1d44a7c6d5ba2b6b59e08ad0eca
Robert James Gabriel <robert_gabriel@outlook.com>
2018-11-11 05:40:39 +0200

    x

--- __core/twitter.js
+++ __core/twitter.js
@@ -1,24 +1,37 @@
-import Twitter from 'twitter';
-import config from './twitter_config/onfig.js';
-import package from '../package.json';
-const T = new Twitter(config);
-let homepage = package.homepage;
-let name = package.name;
-let version = package.version;
+'use strict';
 
-let message = {status: `I just released ${version} of ${name}. Check it out here ${homepage}`};
+var _twitter = require('twitter');
 
-T.post('statuses/update', message, function(error, tweet, response) {
+var _twitter2 = _interopRequireDefault(_twitter);
+
+var _onfig = require('./twitter_config/onfig.js');
+
+var _onfig2 = _interopRequireDefault(_onfig);
+
+var _package = require('../package.json');
+
+var _package2 = _interopRequireDefault(_package);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var T = new _twitter2.default(_onfig2.default);
+var homepage = _package2.default.homepage;
+var name = _package2.default.name;
+var version = _package2.default.version;
+
+var message = { status: 'I just released ' + version + ' of ' + name + '. Check it out here ' + homepage };
+
+T.post('statuses/update', message, function (error, tweet, response) {
     // If the favorite fails, log the error message
     if (error) {
         console.log(err);
     }
     // If the favorite is successful, log the url of the tweet
     else {
-        console.table(tweet);  // Tweet body.
-        console.table(response);  // Raw response object.
-        let username = response.user.screen_name;
-        let tweetId = response.id_str;
-        console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
-    }
+            console.table(tweet); // Tweet body.
+            console.table(response); // Raw response object.
+            var username = response.user.screen_name;
+            var tweetId = response.id_str;
+            console.log('Favorited: ', 'https://twitter.com/' + username + '/status/' + tweetId);
+        }
 });
