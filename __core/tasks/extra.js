707cf0c891b8aca087b3057cd94a7afc06c624f2
Robert James Gabriel <robert_gabriel@outlook.com>
2019-01-07 01:52:19 +0000

    Building js

--- __core/tasks/extra.js
+++ __core/tasks/extra.js
@@ -2,29 +2,27 @@ import del from 'del';
 import gulp from 'gulp';
 
 
-
-
 module.exports = {
   move_app_files: function () {
     return gulp
       .src([
-        '../../app/views/apps/**/*.*',
-        '!../../app/views/apps/**/*.pug'
+        './app/views/apps/**/*.*',
+        '!./app/views/apps/**/*.pug'
       ])
       .pipe(gulp.dest('./dist/apps'));
   },
   clean: function () {
-    return del.sync(['!../../dist/CNAME', '../../dist/*'], {
+    return del.sync(['!./dist/CNAME', './dist/*'], {
       force: true
     });
   },
   build_downloads: function () {
     return gulp
-      .src('../../app/assets/downloads/**/*.*')
-      .pipe(gulp.dest('../../dist/assets/downloads/'));
+      .src('./app/assets/downloads/**/*.*')
+      .pipe(gulp.dest('./dist/assets/downloads/'));
   },
   extra: function () {
-    return gulp.src('../../__core/files/*').pipe(gulp.dest('../../dist/'))
+    return gulp.src('./__core/files/*').pipe(gulp.dest('./dist/'))
   }
 
 };
--- app/assets/js/app/vue/navigation.js
+++ app/assets/js/app/vue/navigation.js
@@ -4,21 +4,22 @@ var repos = new Vue({
         isOpen: false,
     },
     created: function () {
-        var WebP
-        WebP = new Image()
+        console.log('hoooo');
+        var WebP;
+        WebP = new Image();
         WebP.onload = WebP.onerror = function () {
-            var s, sc
+            var s, sc;
             if (WebP.height !== 2) {
-                sc = document.createElement('script')
-                sc.type = 'text/javascript'
-                sc.async = true
-                s = document.getElementsByTagName('script')[0]
-                sc.src = 'assets/js/webpjs.min.js'
-                s.parentNode.insertBefore(sc, s)
+                sc = document.createElement('script');
+                sc.type = 'text/javascript';
+                sc.async = true;
+                s = document.getElementsByTagName('script')[0];
+                sc.src = 'assets/js/webpjs.min.js';
+                s.parentNode.insertBefore(sc, s);
             }
         }
         WebP.src =
-            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 
     },
     methods: {
@@ -35,30 +36,30 @@ var repos = new Vue({
                 // Provided expression must evaluate to a function.
                 if (typeof binding.value !== 'function') {
                     const compName = vNode.context.name
-                    let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
+                    let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`;
                     if (compName) {
-                        warn += `Found in component '${compName}'`
+                        warn += `Found in component '${compName}'`;
                     }
 
                     console.warn(warn)
                 }
                 // Define Handler and cache it on the element
-                const bubble = binding.modifiers.bubble
+                const bubble = binding.modifiers.bubble;
                 const handler = (e) => {
                     if (bubble || (!el.contains(e.target) && el !== e.target)) {
-                        binding.value(e)
+                        binding.value(e);
                     }
                 }
-                el.__vueClickOutside__ = handler
+                el.__vueClickOutside__ = handler;
 
                 // add Event Listeners
-                document.addEventListener('click', handler)
+                document.addEventListener('click', handler);
             },
 
             unbind: function (el, binding) {
                 // Remove Event Listeners
-                document.removeEventListener('click', el.__vueClickOutside__)
-                el.__vueClickOutside__ = null
+                document.removeEventListener('click', el.__vueClickOutside__);
+                el.__vueClickOutside__ = null;
 
             }
         }
