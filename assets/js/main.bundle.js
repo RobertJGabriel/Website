e639b7900555a2723aba916f729b6a23f0485f3f
RobertJGabriel <robert_gabriel@outlook.com>
2025-07-25 16:29:30 +0000

    Deploying to gh-pages from @ RobertJGabriel/Website@e313dd6dcd94a821f9af3df3f5de3ab0a27b1b23 🚀

--- assets/js/main.bundle.js
+++ assets/js/main.bundle.js
@@ -500,6 +500,9 @@ function load() {
 }
  // a list of exported variables
 ;// ./assets/js/index.js
+function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t["return"] || t["return"](); } finally { if (u) throw o; } } }; }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
 // css
 
 
@@ -509,7 +512,31 @@ function load() {
 load();
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js').then(function (registration) {})["catch"](function (registrationError) {});
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        var _iterator = _createForOfIteratorHelper(registrations),
+          _step;
+        try {
+          for (_iterator.s(); !(_step = _iterator.n()).done;) {
+            var registration = _step.value;
+            registration.unregister().then(function () {
+              console.log("🧹 Old service worker unregistered");
+            });
+          }
+
+          // After clearing, register the new one
+        } catch (err) {
+          _iterator.e(err);
+        } finally {
+          _iterator.f();
+        }
+        navigator.serviceWorker.register("/service-worker.js").then(function () {
+          return console.log("✅ New service worker registered");
+        })["catch"](function (err) {
+          return console.log("Service worker error:", err);
+        });
+      });
+    }
   });
 }
 
