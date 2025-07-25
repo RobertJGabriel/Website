e313dd6dcd94a821f9af3df3f5de3ab0a27b1b23
Robert James Gabriel <robert_gabriel@outlook.com>
2025-07-25 11:27:51 -0500

    s

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -10,9 +10,20 @@ app.load();
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('/service-worker.js')
-			.then((registration) => {})
-			.catch((registrationError) => {});
+		    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.getRegistrations().then((registrations) => {
+          for (const registration of registrations) {
+            registration.unregister().then(() => {
+              console.log("🧹 Old service worker unregistered");
+            });
+          }
+
+          // After clearing, register the new one
+          navigator.serviceWorker
+            .register("/service-worker.js")
+            .then(() => console.log("✅ New service worker registered"))
+            .catch((err) => console.log("Service worker error:", err));
+        });
+      }
 	});
 }
